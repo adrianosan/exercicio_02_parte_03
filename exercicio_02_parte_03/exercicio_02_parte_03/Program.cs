c0a497008075491d94db03f4539eae58b2a8784b
adriano.santos <adriano.santos@squadra.com.br>
2019-05-30 11:48:52 -0300

    exercicio_02_parte_ 03

---
+++ exercicio_02_parte_03/exercicio_02_parte_03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_02_parte_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeiroNumero = 0;
+            int segundoNumero = 0;
+            double resultadoTotal = 0;
+            Console.Write("Digite o primeiro numero. ");
+            primeiroNumero = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo numero. ");
+            segundoNumero = int.Parse(Console.ReadLine());
+            resultadoTotal = primeiroNumero / segundoNumero;
+            Console.WriteLine("resultado da divisao é: {0}", resultadoTotal);
+            
+
+            
+        }
+    }
+}
